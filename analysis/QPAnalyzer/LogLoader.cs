fb1e9cb631c1c1a417cf30a067e9aa51a12e63b9
Scott W Harden <swharden@gmail.com>
2020-12-14 22:06:41 -0500

    new analysis plots

--- analysis/QPAnalyzer/LogLoader.cs
+++ analysis/QPAnalyzer/LogLoader.cs
@@ -34,6 +34,40 @@ namespace QPAnalyzer
             IDs = LogDays.SelectMany(x => x.GetIDs()).Distinct().ToArray();
             Days = LogDays.Select(x => x.DateTime.ToOADate()).ToArray();
             Console.WriteLine($"Total IDs: {IDs.Length}");
+
+            Save();
+        }
+
+        public void Save()
+        {
+            string[] ids = LogDays.SelectMany(x => x.GrabsByID.Keys).Distinct().ToArray();
+            Array.Sort(ids);
+
+            DateTime[] dates = LogDays.Select(x => x.DateTime).ToArray();
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("id," + string.Join(',', dates.Select(x => x.ToString())));
+
+            foreach (string id in ids)
+            {
+                sb.Append(id + ",");
+                foreach (var logDay in LogDays)
+                {
+                    int grabs = logDay.GrabsByID.ContainsKey(id) ? logDay.GrabsByID[id] : 0;
+                    sb.Append(grabs + ",");
+                }
+                sb.AppendLine("");
+            }
+
+            System.IO.File.WriteAllText("output/grabs.csv", sb.ToString());
+        }
+
+        public (double[] days, double[] unique) GetUniquePerDay(string id)
+        {
+            double[] counts = new double[Days.Length];
+            for (int i = 0; i < counts.Length; i++)
+                counts[i] = LogDays[i].GrabsByID.ContainsKey(id) ? LogDays[i].GrabsByID[id] : 0;
+            return (Days, counts);
         }
     }
 }
--- analysis/QPAnalyzer/Program.cs
+++ analysis/QPAnalyzer/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace QPAnalyzer
 {
@@ -17,6 +18,8 @@ namespace QPAnalyzer
 
             PlotGrabsPerDay(loader);
             PlotStationsPerDay(loader);
+            PlotLeaders(loader);
+            PlotEachGrabber(loader);
         }
 
         static void ClearOutputFolder()
@@ -37,23 +40,9 @@ namespace QPAnalyzer
 
         static void Leaderboard(LogLoader loader)
         {
-            Dictionary<string, int> totals = new Dictionary<string, int>();
-            foreach (var log in loader.LogDays)
-            {
-                if (log.DateTime.Year != 2020)
-                    continue;
-
-                foreach(string id in log.GrabsByID.Keys)
-                {
-                    if (!totals.ContainsKey(id))
-                        totals.Add(id, 0);
-                    totals[id] = totals[id] + log.GrabsByID[id];
-                }
-            }
-
-            List<KeyValuePair<string, int>> leaders = totals.OrderBy(d => d.Value).Reverse().ToList();
-            string[] leaderLines = leaders.Select(x => $"{x.Key},{x.Value}").ToArray();
-            System.IO.File.WriteAllLines("output/leaders.csv", leaderLines);
+            (string[] ids, int[] counts) = GetLeaders(loader, only2020: true);
+            var lines = Enumerable.Range(0, ids.Length).Select(x => $"{ids[x]},{counts[x]}");
+            System.IO.File.WriteAllLines("output/leaders.csv", lines);
         }
 
         static void PlotGrabsPerDay(LogLoader loader)
@@ -80,6 +69,69 @@ namespace QPAnalyzer
             plt.YLabel("Stations / Day");
             plt.Axis(y1: 0);
             plt.SaveFig("output/grabbers-per-day.png");
+        }
+
+        static void PlotLeaders(LogLoader loader, int limit = 50)
+        {
+            (string[] ids, int[] counts) = GetLeaders(loader, only2020: true);
+            double[] xs = ScottPlot.DataGen.Consecutive(limit);
+            ids = ids.Take(limit).ToArray();
+            double[] counts2 = counts.Take(limit).Select(x => (double)x).ToArray();
+
+            var plt = new ScottPlot.Plot(800, 400);
+            plt.PlotBar(xs, counts2);
+            plt.Title("Most Active QRSS Grabbers in 2020");
+            plt.YLabel("Unique Grabs in 2020");
+            plt.AxisAuto();
+            plt.TightenLayout();
+            plt.Axis(x1: -.5, x2: limit - .25, y1: 0);
+            plt.XTicks(ids);
+            plt.Ticks(xTickRotation: 90);
+            plt.Grid(enableVertical: false);
+            plt.Layout(xLabelHeight: 110, yScaleWidth: 50);
+            plt.SaveFig("output/grabbers-leader.png");
+        }
+
+        static void PlotEachGrabber(LogLoader loader)
+        {
+            foreach (string id in loader.IDs)
+            {
+                (double[] dates, double[] unique) = loader.GetUniquePerDay(id);
+                int total = (int)unique.Sum();
+                if (total < 1000)
+                    continue;
+
+                var plt = new ScottPlot.Plot(800, 400);
+                plt.PlotScatter(dates, unique);
+                plt.Title($"QRSS Grabber Activity for {id} ({total:N0} total grabs)");
+                plt.YLabel("Unique Grabs / Day");
+                plt.Axis(y1: 0, y2: 160);
+                plt.Ticks(dateTimeX: true);
+                plt.SaveFig($"output/individual-{id}.png");
+                Console.WriteLine($"Saved graph for: {id}");
+            }
+        }
+
+        static (string[] ids, int[] counts) GetLeaders(LogLoader loader, bool only2020)
+        {
+            Dictionary<string, int> totals = new Dictionary<string, int>();
+            foreach (var log in loader.LogDays)
+            {
+                if (only2020 && log.DateTime.Year != 2020)
+                    continue;
+
+                foreach (string id in log.GrabsByID.Keys)
+                {
+                    if (!totals.ContainsKey(id))
+                        totals.Add(id, 0);
+                    totals[id] = totals[id] + log.GrabsByID[id];
+                }
+            }
+
+            List<KeyValuePair<string, int>> leaders = totals.OrderBy(d => d.Value).Reverse().ToList();
+            string[] ids = leaders.Select(x => x.Key).ToArray();
+            int[] counts = leaders.Select(x => x.Value).ToArray();
+            return (ids, counts);
         }
     }
 }
