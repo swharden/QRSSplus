750e369f1c7577a25a255c83fb1f00cefa90d9c3
Andy G0FTD <42384716+g0ftd@users.noreply.github.com>
2020-12-17 19:30:23 +0000

    Update QRSSplusUpdate.py
    Trying -N option for wget.
    Attempt to solve updating problem.

--- QRSSplusUpdate.py
+++ QRSSplusUpdate.py
@@ -113,7 +113,7 @@ class QrssPlus:
         """download a url to temp.dat using wget"""
         if os.path.exists("temp.dat"):
             os.remove("temp.dat")
-        cmd = "wget -q -T 3 -t 1"  # 1 attempt (no retries)
+        cmd = "wget -q -N -T 3 -t 1"  # 1 attempt (no retries)
         cmd += " -O %s %s" % ("temp.dat", url+"?"+str(int(time.time())))
         self.log(cmd)
         process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
