d44d86237f3ce1fb961662f56dfa5c3b7ce1a2df
Scott W Harden <swharden@gmail.com>
2021-04-17 22:27:18 -0400

    set-up table storage

---
+++ src/azure/QrssPlus/GrabResult.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+
+namespace QrssPlus
+{
+    public class GrabResult : TableEntity
+    {
+        public string GrabberID { get; init; }
+        public string Hash { get; init; }
+        public int ResultCode { get; init; }
+        public double ResultTime { get; init; }
+
+        public GrabResult(string id, string hash, int code, double time)
+        {
+            PartitionKey = "GrabResult";
+            string timestamp = DateTime.UtcNow.ToString("s", System.Globalization.DateTimeFormatInfo.InvariantInfo);
+            RowKey = $"{id} {timestamp}";
+            GrabberID = id;
+            Hash = hash;
+            ResultCode = code;
+            ResultTime = time;
+        }
+    }
+}
---
+++ src/azure/QrssPlus/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QrssPlus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(GetStorageConnectionString());
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable table = tableClient.GetTableReference("test2");
+            table.CreateIfNotExists();
+
+            var grab = new GrabResult("testID", "testHash", 123, 12.345);
+
+            TableOperation insertGrab = TableOperation.InsertOrMerge(grab);
+            table.Execute(insertGrab);
+
+            Console.WriteLine("DONE");
+        }
+
+        public static string GetStorageConnectionString()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddUserSecrets<GrabberInfo>()
+                .AddEnvironmentVariables()
+                .Build();
+
+            string connectionString = configuration["StorageConnectionString"];
+            if (string.IsNullOrEmpty(connectionString))
+                throw new InvalidOperationException("cannot find connection string");
+
+            return connectionString;
+        }
+    }
+}
