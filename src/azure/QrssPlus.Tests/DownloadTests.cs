d4b7a37d572fa1c966cf98888bfce177667dbbe0
Scott W Harden <swharden@gmail.com>
2021-04-19 20:12:38 -0400

    add support for MD5 hashing

--- src/azure/QrssPlus.Tests/DownloadTests.cs
+++ src/azure/QrssPlus.Tests/DownloadTests.cs
@@ -5,14 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Net;
 
 namespace QrssPlus.Tests
 {
-    [Ignore("Skip Local Download Tests")]
     class DownloadTests
     {
         [Test]
-        public void Test_Download_Locally()
+        [Ignore("skip large download tests")]
+        public void Test_Download_ImageFiles()
         {
             var grabbers = GrabberListFactory.CreateFromCsvFile(SampleData.GrabbersCsvPath);
             string localFolder = Path.GetFullPath("./downloads");
@@ -20,6 +21,18 @@ namespace QrssPlus.Tests
             Web.DownloadAllLocally(grabbers, localFolder, maxCount: 10);
 
             Assert.NotZero(Directory.GetFiles(localFolder).Length);
+        }
+
+        [Test]
+        public void Test_Download_Hash()
+        {
+            using WebClient client = new();
+
+            byte[] data = client.DownloadData(SampleData.SampleImageUrl);
+            Assert.AreEqual(SampleData.SampleImageSize, data.Length);
+
+            string hash = GrabData.Hash(data);
+            Assert.AreEqual(SampleData.SampleImageHash, hash);
         }
     }
 }
--- src/azure/QrssPlus.Tests/SampleData.cs
+++ src/azure/QrssPlus.Tests/SampleData.cs
@@ -10,5 +10,9 @@ namespace QrssPlus.Tests
     {
         public const string GrabbersCsvPath = "../../../data/grabbers.csv";
         public const string GrabbersCsvUrl = "https://raw.githubusercontent.com/swharden/QRSSplus/master/grabbers.csv";
+
+        public const string SampleImageUrl = "https://homepages.cae.wisc.edu/~ece533/images/baboon.png";
+        public const int SampleImageSize = 637192;
+        public const string SampleImageHash = "674fd8e5a649dd8eb182d868b4c6d1b8";
     }
 }
