360c931b1f9b560409f2fd7855fb073943b46301
Scott W Harden <swharden@gmail.com>
2021-04-25 19:09:24 -0400

    QRSS Plus Viewer with Blazor

---
+++ src/azure/QrssPlusViewer/GrabberStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QrssPlusViewer
+{
+    public class GrabberStatus
+    {
+        public string ID;
+        public string Name;
+        public string Callsign;
+        public string Location;
+        public string ImageUrl;
+        public string SiteUrl;
+        public double Age;
+        public string[] GrabUrls;
+        public bool IsActive => GrabUrls.Length > 0;
+    }
+}
