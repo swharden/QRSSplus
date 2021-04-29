7090716fdf79c67029a5987c617cf9d1c438b966
Scott W Harden <swharden@gmail.com>
2021-04-28 20:04:12 -0400

    improve image data verification

--- src/azure/QrssPlus/GrabberData.cs
+++ src/azure/QrssPlus/GrabberData.cs
@@ -23,8 +23,13 @@ namespace QrssPlus
             try
             {
                 Bytes = client.DownloadData(info.ImageUrl);
+
+                if (Bytes is null || Bytes.Length == 0)
+                    throw new WebException("Image URL contains no data");
+
                 if (Bytes[0] == '<')
-                    throw new WebException("Image URL points to a HTML page, not an image file");
+                    throw new WebException("Image URL contains HTML (not an image)");
+
                 Hash = GetHash(Bytes);
                 Response = "success";
                 string timestamp = $"{dt.Year:D2}.{dt.Month:D2}.{dt.Day:D2}.{dt.Hour:D2}.{dt.Minute:D2}.{dt.Second:D2}";
