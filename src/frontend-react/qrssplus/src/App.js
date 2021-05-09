13950762b51d02c0cafb59a3028e5b06ff38981a
Scott W Harden <swharden@gmail.com>
2021-05-09 13:44:16 -0400

    list grabber IDs

--- src/frontend-react/qrssplus/src/App.js
+++ src/frontend-react/qrssplus/src/App.js
@@ -1,35 +1,47 @@
 import React from 'react';
-import Clock from './Clock';
-import Dashboard from './Dashboard';
 
 class App extends React.Component {
+
   constructor(props) {
     super(props);
-    this.state = { grabbersJson: "update needed" };
-    this.dashboardRef = React.createRef();
+    this.state = {
+      grabbersJson: {},
+      grabbers: {}
+    };
   }
 
-  updateGrabbers() {
-    this.setState({ grabbersJson: "updating..." });
+  onUpdateGrabbers() {
+    this.setState({ grabbersJson: "downloading..." });
     fetch('https://qrssplus.z20.web.core.windows.net/grabbers.json')
       .then(response => response.json())
-      .then(data => this.gotUpdatedJson(data));
+      .then(obj => {
+        this.setState({ grabbersJson: obj });
+        this.setState({ grabbers: obj.grabbers });
+      });
   }
-
-  gotUpdatedJson(data) {
-    this.dashboardRef.current.update(data);
-  }
-
 
   render() {
     return (
       <div className="container">
         <h1>QRSS Plus</h1>
         <h3>Automatically Updating Active QRSS Grabbers List</h3>
-        <Clock />
-        <button onClick={this.updateGrabbers.bind(this)}>update</button>
-        <pre>{this.state.grabbersJson}</pre>
-        <Dashboard ref={this.dashboardRef} />
+
+        <div className="bg-dark text-light shadow border rounded m-5 p-3">
+          <h1>Component Test</h1>
+          <button onClick={this.onUpdateGrabbers.bind(this)}>update</button>
+
+          <div>
+            <ul>
+              {
+                Object.keys(this.state.grabbers).map(id => (
+                  <li>{id}</li>
+                ))
+              }
+            </ul>
+          </div>
+
+        </div>
+
       </div>
     );
   }
