4a3ec49b8ee638ee40657dde2ac8e0e0e0e4a2c8
Scott W Harden <swharden@gmail.com>
2021-05-11 19:24:21 -0400

    add options to reduce initial load

--- src/frontend-react/qrssplus/src/App.js
+++ src/frontend-react/qrssplus/src/App.js
@@ -9,8 +9,12 @@ class App extends React.Component {
       grabbers: {},
       timeNow: new Date(),
       lastUpdate: null,
-      nextUpdate: null
+      nextUpdate: null,
+      isAutoUpdateEnabled: true,
+      thumbnailHistory: "2hr",
+      isStichVisible: false,
     };
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   componentDidMount() {
@@ -19,6 +23,11 @@ class App extends React.Component {
 
   tick() {
     const dt = new Date();
+
+    if (!this.state.isAutoUpdateEnabled) {
+      this.setState({ timeNow: dt });
+      return;
+    }
 
     if (dt >= this.state.nextUpdate) {
       this.onUpdateGrabbers();
@@ -86,15 +95,83 @@ class App extends React.Component {
       + this.leftPad(dt.getUTCSeconds()) + " UTC";
   }
 
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
   /**
    * section at the top of the page
    */
   renderTimer() {
     return (
-      <div className="d-inline-block bg-light border rounded p-2 m-3">
-        <div>Current Time: <code>{this.getTimestamp(this.state.timeNow)}</code></div>
-        <div>Last Update: <code>{this.getTimestamp(this.state.lastUpdate)}</code></div>
-        <div>Next Update: <code>{this.getTimestamp(this.state.nextUpdate)}</code></div>
+      <div className="my-4">
+
+        <div className="d-inline-block bg-light border rounded p-2 m-3 align-top">
+          <div>Current Time: <code>{this.getTimestamp(this.state.timeNow)}</code></div>
+          <div>Last Update: <code>{this.getTimestamp(this.state.lastUpdate)}</code></div>
+          <div>Next Update: <code>{this.state.isAutoUpdateEnabled ? this.getTimestamp(this.state.nextUpdate) : "disabled"}</code></div>
+          <div className="form-check mt-2">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              name="isAutoUpdateEnabled"
+              id="isAutoUpdateEnabled"
+              checked={this.state.isAutoUpdateEnabled}
+              onChange={this.handleInputChange}
+            />
+            Auto-Update
+          </div>
+        </div>
+
+        <div className="d-inline-block bg-light border rounded p-2 m-3 align-top">
+
+          <div className="">
+            Thumbnails:
+        </div>
+
+          <select className="form-select" defaultValue={'2hr'} name="thumbnailHistory" onChange={this.handleInputChange}>
+            <option value="none">None</option>
+            <option value="2hr">2hr (12)</option>
+            <option value="8hr">8hr (48)</option>
+          </select>
+
+          <div className="form-check mt-3">
+            <input className="form-check-input" type="checkbox" name="isStichVisible" onChange={this.handleInputChange} />
+            8hr stitch
+          </div>
+        </div>
+
+        <div className="d-inline-block bg-light border rounded p-2 m-3 align-top">
+          <div className="mb-2">
+            QRSS Plus:
+        </div>
+          <ul>
+            <li><a href="https://swharden.com/blog/2020-10-03-new-age-of-qrss/#qrss-frequency-bands">QRSS Frequency
+              Bands</a></li>
+            <li><a href="https://github.com/swharden/qrssplus/#request-a-change">Suggest a Grabber</a></li>
+            <li><a href="https://github.com/swharden/qrssplus/#request-a-change">Request a Modification</a></li>
+          </ul>
+        </div>
+
+        <div className="d-inline-block bg-light border rounded p-2 m-3 align-top">
+          <div className="mb-2">
+            Additional Resources:
+        </div>
+          <ul>
+            <li><a href="https://groups.io/g/qrssknights">Knights QRSS Mailing List</a></li>
+            <li><a href='https://swharden.com/qrss/74/'>74!, The Knights QRSS Newsletter</a></li>
+
+            <li><a href="https://swharden.com/blog/2020-10-03-new-age-of-qrss">The
+              New Age of QRSS</a></li>
+          </ul>
+        </div>
+
       </div>
     )
   }
@@ -156,7 +233,7 @@ class App extends React.Component {
     )
   }
 
-  renderDetailsForGrabber(grabber) {
+  renderDetailsForGrabber(grabber, maxThumbnailCount, showStitch) {
     const latestUrl = grabber.urls[grabber.urls.length - 1];
     return (
       <div id={grabber.id} key={latestUrl}>
@@ -188,23 +265,36 @@ class App extends React.Component {
           Object.keys(grabber.urls)
             .map(x => grabber.urls[x])
             .reverse()
+            .slice(0, maxThumbnailCount)
             .map(x => this.renderDatedThumbnail(x))
         }
 
-        <div className="text-nowrap overflow-scroll m-2 bg-light border shadow" >
-          {
-            Object.keys(grabber.urls)
-              .map(x => grabber.urls[x])
-              .map(url => (
-                <a href={url} key={url}>
-                  <img src={url + "-thumb-skinny.jpg"} alt={url} width="50" height="500" />
-                </a>
-              ))
-          }
-        </div>
+        {
+          showStitch ? (
+            <div className="text-nowrap overflow-scroll m-2 bg-light border shadow" >
+              {
+                Object.keys(grabber.urls)
+                  .map(x => grabber.urls[x])
+                  .map(url => (
+                    <a href={url} key={url}>
+                      <img src={url + "-thumb-skinny.jpg"} alt={url} width="50" height="500" />
+                    </a>
+                  ))
+              }
+            </div>
+          ) : ""
+        }
 
       </div>
     );
+  }
+
+  getThumbnailCount() {
+    if (this.state.thumbnailHistory === "8hr")
+      return 6 * 8;
+    if (this.state.thumbnailHistory === "2hr")
+      return 6 * 2;
+    return 0;
   }
 
   renderDetailsForAllGrabbers() {
@@ -213,7 +303,7 @@ class App extends React.Component {
         {
           Object.keys(this.state.grabbers)
             .filter(id => this.state.grabbers[id].urls.length > 0)
-            .map(id => this.renderDetailsForGrabber(this.state.grabbers[id]))
+            .map(id => this.renderDetailsForGrabber(this.state.grabbers[id], this.getThumbnailCount(), this.state.isStichVisible))
         }
       </section>
     )
