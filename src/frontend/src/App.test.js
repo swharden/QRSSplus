7e3ab616360ddf03b301435d9bef1f79a5bafb44
Scott W Harden <swharden@gmail.com>
2021-12-19 01:36:49 -0500

    frontend: update test names

--- src/frontend/src/App.test.js
+++ src/frontend/src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app without crashing', () => {
   render(<App />);
 });
